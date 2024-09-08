e8dbb95d69287dd8f3da3fc0031e0b1a01f15c0a
tiez <2268841928@qq.com>
2024-09-08 14:12:08 +0800

    test Connection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<bits/stdc++.h>
+#include "Connection.h"
 using namespace std;
 
 int main()
 {
+	Connection conn;
+	char sql[1024] = { 0 };
+	sprintf(sql, "insert into user(name,password,state) values('%s','%s','%s')", "wang wu", "123456", "offline");
+	conn.connect("127.0.0.1", 3306, "root", "lpy20030123", "chat");
+	conn.update(sql);
 	return 0; 
 }
